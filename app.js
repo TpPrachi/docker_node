3d27c01d722122feb1a4e71bb780cd778b30bbf4
Prachi Thakkar <prachi.thakkar@solutionanalysts.com>
2020-07-04 13:26:22 +0530

    [Fix] Remove dynamo db from app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const AWS = require('aws-sdk');
-const uuid = require('uuid');
+// const AWS = require('aws-sdk');
+// const uuid = require('uuid');
 
-AWS.config = {
-  accessKeyId: 'AKIAI52YBKRMVODPRSHQ',
-  secretAccessKey: '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',
-  region: 'us-east-1',
-  signatureVersion: 'v4',
-};
+// AWS.config = {
+//   accessKeyId: 'AKIAI52YBKRMVODPRSHQ',
+//   secretAccessKey: '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',
+//   region: 'us-east-1',
+//   signatureVersion: 'v4',
+// };
 
 // Set the region 
 // AWS.config.update({region: 'us-east-1'});
 
 // Create the DynamoDB service object
-var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+// var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-var params = {
-  TableName: 'CUSTOMER_LIST',
-  Item: {
-    'CUSTOMER_ID' : { S: '001' },
-    'CUSTOMER_NAME' : { S: 'Richard Roe' }
-  }
-};
+// var params = {
+//   TableName: 'CUSTOMER_LIST',
+//   Item: {
+//     'CUSTOMER_ID' : { S: '001' },
+//     'CUSTOMER_NAME' : { S: 'Richard Roe' }
+//   }
+// };
 
 // Call DynamoDB to add the item to the table
-ddb.putItem(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data);
-  }
-});
+// ddb.putItem(params, function(err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log("Success", data);
+//   }
+// });
 
 const path = __dirname + '/views/';
 const port = 5000;
